2201e02c801dd727b3b69e1516c514d6b7208247
mydimons <mydimons.kb@gmail.com>
2025-10-15 02:02:01 -0700

    new input manager system
    NOT WORKING (yet)

--- Pong+/Assets/Scripts/Paddles/Paddle.cs
+++ Pong+/Assets/Scripts/Paddles/Paddle.cs
@@ -1,17 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputManager;
 
+[RequireComponent(typeof(PlayerPaddle))]
 public class Paddle : MonoBehaviour
 {
     [Header("Player Controlled Paddle")]
     public bool playerControlledPaddle;
     [Tooltip("True = Left Player, False = Right Player (Used for controls)")]
     public bool leftPlayerControls;
+    InputManager playerPaddleControls; //wtf
 
     [Header("AI Controlled Paddle")]
     public bool aiControlledPaddle;
+
+    // OTHER
+    PlayerPaddle playerPaddle;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        playerPaddle = this.GetComponent<PlayerPaddle>();
+
         // check for error between bools
         if (playerControlledPaddle && aiControlledPaddle)
         {
@@ -22,6 +31,33 @@ public class Paddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerControlledPaddle)
+        {
+            PlayerControlledPaddle();
+        }
+        else if (aiControlledPaddle)
+        {
+            AIControlledPaddle();
+        }
+    }
+
+    void PlayerControlledPaddle()
+    {
+        float input;
+
+        if (leftPlayerControls)
+        {
+            input = 1;
+        }
+        else
+        {
+            input = 1;
+        }
+
+    }
+
+    void AIControlledPaddle()
+    {
+
     }
 }
---
+++ Pong+/Assets/Scripts/Paddles/PlayerPaddle.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Paddle))]
+public class PlayerPaddle : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void MovePaddle(float speed, float input) // input SHOULD be a -1 - 1 range
+    {
+        
+    }
+}
